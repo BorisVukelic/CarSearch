6e6b8dc37ac6b6e1570d4bc993f3168f9d244d0d
BorisVukelic <slobodan.vukelic@gmail.com>
2018-09-24 09:07:12 +0200

    boris

--- carsearch/src/components/CarSearch.vue
+++ carsearch/src/components/CarSearch.vue
@@ -2,7 +2,7 @@
   <div class="Root">
     <div id="wraper">
         <div class="but but1">
-          <select class="js-example-basic-single" name="MAKE">
+          <select class="js-example-basic-single" style="height: 100%!important" name="MAKE">
             <option value="AL">Alabama</option>
             ...
             <option value="WY">Wyoming</option>
@@ -22,95 +22,102 @@
 export default {
 }
 </script>
-
+<style media="screen">
+.but select{
+  height: 100%!important;
+}
+</style>
 <style lang="css" scoped>
 #wraper{
-        display: grid;
-        margin-top: 40px;
-        grid-template-columns: repeat(55,1fr);
-        grid-auto-rows: 70px;
-        margin: 40px auto;
-        width:90%;
-      }
-      .but{
-        border:1px solid black;
-        text-align: center;
-        line-height: 70px;
-        vertical-align: middle;
-      }
-      @media only screen and (min-width: 1110px){
-         .but1{
-          grid-column: 1/8 ;
-        }
-        .but2{
-          grid-column: 8/16 ;
-        }
-        .but3{
-          grid-column: 16/24 ;
-        }
-        .but4{
-          grid-column: 24/32 ;
-        }
-        .but5{
-          grid-column: 32/40 ;
-        }
-        .but6{
-          grid-column: 40/48 ;
-        }
-        .but7{
-          grid-column: 48/56 ;
-        }
-      }
-      @media only screen and (max-width: 1110px){
-         .but1{
-          grid-column: 1/14 ;
-        }
-        .but2{
-          grid-column: 14/28 ;
-        }
-        .but3{
-          grid-column: 28/42 ;
-        }
-        .but4{
-          grid-column: 42/56 ;
-        }
-        .but5{
-          grid-column: 1/21 ;
-        }
-        .but6{
-          grid-column: 21/42 ;
-        }
-        .but7{
-          grid-column: 42/56 ;
-        }
-        #wraper{
-          width:790px;
-        }
-      }
-      @media only screen and (max-width: 798px){
-         .but1{
-          grid-column: 1/56 ;
-        }
-        .but2{
-          grid-column: 1/56 ;
-        }
-        .but3{
-          grid-column: 1/56 ;
-        }
-        .but4{
-          grid-column: 1/56 ;
-        }
-        .but5{
-          grid-column: 1/56 ;
-        }
-        .but6{
-          grid-column: 1/56 ;
-        }
-        .but7{
-          grid-column: 1/56 ;
-        }
-        #wraper{
-          width:350px;
-        }
-      }
+  display: grid;
+  margin-top: 40px;
+  grid-template-columns: repeat(55,1fr);
+  grid-auto-rows: 70px;
+  margin: 40px auto;
+  width:90%;
+  border: 1px solid #777;
+  border-radius: 5px;
+  overflow-y: hidden;
+}
+.but{
+  text-align: center;
+  line-height: 70px;
+  vertical-align: middle;
+}
+@media only screen and (min-width: 1110px){
+.but1{
+  grid-column: 1/8 ;
+}
+.but2{
+  grid-column: 8/16 ;
+}
+.but3{
+  grid-column: 16/24 ;
+}
+.but4{
+  grid-column: 24/32 ;
+}
+.but5{
+  grid-column: 32/40 ;
+}
+.but6{
+  grid-column: 40/48 ;
+}
+.but7{
+  grid-column: 48/56 ;
+}
+}
+@media only screen and (max-width: 1110px){
+.but1{
+  grid-column: 1/14 ;
+}
+.but2{
+  grid-column: 14/28 ;
+}
+.but3{
+  grid-column: 28/42 ;
+}
+.but4{
+  grid-column: 42/56 ;
+}
+.but5{
+  grid-column: 1/21 ;
+}
+.but6{
+  grid-column: 21/42 ;
+}
+.but7{
+  grid-column: 42/56 ;
+}
+#wraper{
+  width:790px;
+}
+}
+
+@media only screen and (max-width: 798px){
+.but1{
+  grid-column: 1/56 ;
+}
+.but2{
+  grid-column: 1/56 ;
+}
+.but3{
+  grid-column: 1/56 ;
+}
+.but4{
+  grid-column: 1/56 ;
+}
+.but5{
+  grid-column: 1/56 ;
+}
+.but6{
+  grid-column: 1/56 ;
+}
+.but7{
+  grid-column: 1/56 ;
+}
+#wraper{
+  width:350px;
+}
+}
 </style>
